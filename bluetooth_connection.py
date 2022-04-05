96b80b8b83f208d4eae94a964eb8c16f179edb4d
LolaSi <72922569+LolaSi@users.noreply.github.com>
2022-04-05 18:30:08 +0300

    Update bluetooth_connection.py

--- bluetooth_connection.py
+++ bluetooth_connection.py
@@ -1,24 +1,35 @@
 import bluetooth
 
-server_socket=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
+server_sock.listen(1)
 
-port=1
+port = server_sock.getsockname()[1]
 
-server_socket.bind(("",port))
-server_socket.listen(1)
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-client_socket, address = server_socket.accept()
-print "Accepted Connection from ", address
+bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                            # protocols=[bluetooth.OBEX_UUID]
+                            )
 
-while true:
-    data = client_socket.recv(1024)
-    print "Received: %s" % data
-    if (data == "0"):    #if '0' is sent from the Android App, send picture 
-        print ("sending pic")
-    if (data == "q"):
-        print ("Quit")
-        break
- 
-client_socket.close()
-server_socket.close()
+print("Waiting for connection on RFCOMM channel", port)
 
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if not data:
+            break
+        print("Received", data)
+except OSError:
+    pass
+
+print("Disconnected.")
+
+client_sock.close()
+server_sock.close()
+print("All done.")
