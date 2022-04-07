448a28d2307a98cab7809d624b1365681f3f1a8b
LolaSi <72922569+LolaSi@users.noreply.github.com>
2022-04-07 17:48:59 +0300

    Update bluetooth_connection.py
    Lola - subprocess is taking picture now

--- bluetooth_connection.py
+++ bluetooth_connection.py
@@ -1,36 +1,52 @@
 import bluetooth
+import subprocess
+import os
 
+def sendPicture(client_sock):
+    print("yay, going to take pic")
+    subprocess.run(["python", "still_pic.py"])
+    f = open('./photos/image.jpg', 'rb')
+    l = os.path.getsize('./photos/image.jpg')
+    m = f.read(l)
+    client_sock.send(m)
+    
+    
 
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server_sock.bind(("", bluetooth.PORT_ANY))
-server_sock.listen(1)
+def main():
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server_sock.bind(("", bluetooth.PORT_ANY))
+    server_sock.listen(1)
 
-port = server_sock.getsockname()[1]
+    port = server_sock.getsockname()[1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
-                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
-                            # protocols=[bluetooth.OBEX_UUID]
-                            )
+    bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                                service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                                profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                                # protocols=[bluetooth.OBEX_UUID]
+                                )
+                                
+    print("Waiting for connection on RFCOMM channel", port)
 
-print("Waiting for connection on RFCOMM channel", port)
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from", client_info)
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from", client_info)
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if data.decode("utf-8") == "1":
+                print("yay, going to take pic")
+                sendPicture(client_sock)
+            if not data:
+                break
+            print("Received", data)
+    except OSError:
+        print("Disconnected.")
 
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if not data:
-            break
-        print("Received", data)
-except OSError:
-    pass
+    client_sock.close()
+    server_sock.close()
+    print("All done.")
 
-print("Disconnected.")
-
-client_sock.close()
-server_sock.close()
-print("All done.")
+if __name__ == '__main__':
+    main()
